6623cb5d0609e75b0d09923022c19bd3cca7f090
JoshMcQuinn <joshmcquinn75@gmail.com>
2017-02-01 16:18:20 -0500

    Start

---
+++ PurdueChallange2.java
@@ -0,0 +1,20 @@
+
+public class PurdueChallange2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(compareCurrency(68));
+		System.out.println(compareCurrency(105));
+		System.out.println(compareCurrency(22));
+
+	}
+	
+	public static boolean compareCurrency(int dol)
+	{
+		//calculate current system sum 
+		int hundred = 100 % dol;
+		System.out.println(hundred);
+		return false;
+	}
+
+}
