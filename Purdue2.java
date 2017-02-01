ed02da3134e97adc7b5874334ce43078669a3532
JoshMcQuinn <joshmcquinn75@gmail.com>
2017-02-01 15:37:56 -0500

    PurdueChallenge#2 Start

---
+++ Purdue2.java
@@ -0,0 +1,18 @@
+
+public class Purdue2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(compareCurrency(68));
+		System.out.println(compareCurrency(105));
+		System.out.println(compareCurrency(22));
+
+	}
+	
+	public static boolean compareCurrency(int dol)
+	{
+		
+	}
+
+}
